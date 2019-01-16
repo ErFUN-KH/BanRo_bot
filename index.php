6cf62a24a89e475a2654d2e9ced67959ee936c09
ErFUN KH <erfun.kh@gmail.com>
2019-01-16 10:12:09 +0330

    Bug fix: added too many bot

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
 
-require (__DIR__ . '/env.php');
+require(__DIR__ . '/env.php');
 
 $result = json_decode(file_get_contents('php://input'));
 
-if (isset($result->message->new_chat_member->is_bot) && $result->message->new_chat_member->is_bot) {
-    kick_user($result->message->new_chat_member->id, $result->message->chat->id);
+
+foreach ($result->message->new_chat_members as $member) {
+    if (isset($member->is_bot) && $member->is_bot) {
+        kick_user($member->id, $member->id);
+    }
 }
 
 function kick_user($user_id, $chat_id)
 {
-    $url = 'https://api.telegram.org/bot'.constant("TOKEN").'/kickChatMember';
+    $url = 'https://api.telegram.org/bot' . constant("TOKEN") . '/kickChatMember';
 
     $postfields["user_id"] = $user_id;
     $postfields["chat_id"] = $chat_id;
