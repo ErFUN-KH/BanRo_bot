90531864d7d37427c97eaf4c7c96047ee93b8720
ErFUN KH <erfun.kh@gmail.com>
2019-01-16 10:19:01 +0330

    Bug fix: added too many bot

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ require(__DIR__ . '/env.php');
 
 $result = json_decode(file_get_contents('php://input'));
 
-
-foreach ($result->message->new_chat_members as $member) {
-    if (isset($member->is_bot) && $member->is_bot) {
-        kick_user($member->id, $member->id);
+if (isset($result->message->new_chat_members[0])) {
+    foreach ($result->message->new_chat_members as $member) {
+        if ($member->is_bot) {
+            kick_user($member->id, $member->id);
+        }
     }
 }
 
@@ -17,6 +18,21 @@ function kick_user($user_id, $chat_id)
 
     $postfields["user_id"] = $user_id;
     $postfields["chat_id"] = $chat_id;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+    curl_exec($ch);
+}
+
+function send_message()
+{
+    $url = 'https://api.telegram.org/bot' . constant("TOKEN") . '/sendMessage';
+
+    $postfields["chat_id"] = 71359656;
+    $postfields["text"] = file_get_contents('php://input');
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
