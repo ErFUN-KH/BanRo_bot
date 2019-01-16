fb875c48e94a63501776d52603cf74c7cc961aa2
ErFUN KH <erfun.kh@gmail.com>
2019-01-16 10:43:05 +0330

    Bug fix: kick bot

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $result = json_decode(file_get_contents('php://input'));
 if (isset($result->message->new_chat_members[0])) {
     foreach ($result->message->new_chat_members as $member) {
         if ($member->is_bot) {
-            kick_user($member->id, $member->id);
+            kick_user($member->id, $result->message->chat->id);
         }
     }
 }
@@ -18,21 +18,6 @@ function kick_user($user_id, $chat_id)
 
     $postfields["user_id"] = $user_id;
     $postfields["chat_id"] = $chat_id;
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-    curl_exec($ch);
-}
-
-function send_message()
-{
-    $url = 'https://api.telegram.org/bot' . constant("TOKEN") . '/sendMessage';
-
-    $postfields["chat_id"] = 71359656;
-    $postfields["text"] = file_get_contents('php://input');
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
